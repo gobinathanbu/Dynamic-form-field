80049abf24bf3a81381e49b76a4aea6d554d097c
Gobi-Chrono <gobinath.chronoinfotech@gmail.com>
2020-04-06 13:23:02 +0530

    form functions updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -82,6 +83,8 @@ import { MatTreeModule } from '@angular/material/tree';
     MatToolbarModule,
     MatTooltipModule,
     MatTreeModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form-field/form-field.component.ts
+++ src/app/components/form-field/form-field.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'app-form-field',
-  templateUrl: './form-field.component.html',
-  styleUrls: ['./form-field.component.css']
+    selector: 'app-form-field',
+    templateUrl: './form-field.component.html',
+    styleUrls: ['./form-field.component.css']
 })
 export class FormFieldComponent implements OnInit {
+    dynamicForm: FormGroup;
+    constructor(private _fb: FormBuilder) { }
 
-  constructor() { }
+    get educationArray() {
+      return this.dynamicForm.get('education') as FormArray;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.dynamicForm = this._fb.group({
+            name: new FormControl(''),
+            age: new FormControl(''),
+            phoneNumber: new FormControl(''),
+            email: new FormControl(''),
+            education: this._fb.array([this.educationGroup()])
+        });
+    }
 
+    educationGroup() {
+      return this._fb.group({
+        course: new FormControl(''),
+        institute: new FormControl(''),
+        city: new FormControl(''),
+      });
+    }
+
+    addCourse() {
+      this.educationArray.push(this.educationGroup());
+    }
+    removeCourse(id) {
+      this.educationArray.removeAt(id)
+    }
+    courseDetails() {
+      const value = this.dynamicForm.value;
+      console.log(value);
+    }
 }
